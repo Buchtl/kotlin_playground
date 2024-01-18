7e1911f6be0d40977f178608f429cf330d416dbe
Chris <schauer@inf-schauer.de>
2024-01-18 20:31:04 +0100

    fix: remove at index

--- app/src/main/kotlin/kotlin_playground/App.kt
+++ app/src/main/kotlin/kotlin_playground/App.kt
@@ -20,10 +20,9 @@ fun main() {
   val data = ObjectWithValidity(validFrom = null, validTo = LocalDateTime.now())
   println(data.validTo)
   val index = 5
-  val values: List<Int> = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9).also{ it.remove(it.indexOf(index+1)) }
+  val values: List<Int> = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9).also{ it.removeAt(it.indexOf(index)) }
   println("Split at index $index ")
   val grouped = values.groupBy { it > 5 }
   println("group true: ${grouped[true]}")
   println("group false: ${grouped[false]?.reversed() }")
-
 }
