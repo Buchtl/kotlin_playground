e533f53725b8206f0cb36f62bd3f960e72ee3efb
Chris <schauer@inf-schauer.de>
2024-01-18 19:43:32 +0100

    refactor

--- app/src/main/kotlin/kotlin_playground/date/DateUtils.kt
+++ app/src/main/kotlin/kotlin_playground/date/DateUtils.kt
@@ -4,15 +4,23 @@ import java.time.LocalDateTime
 
 class DateUtils {
 
-  fun normalizeDate(date: LocalDateTime): String {
-    val pattern = "[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}"
-    val regex = Regex(pattern)
-    return regex.find(date.toString())?.value ?: ""
+  fun normalizeDate(date: String): String {
+    val regex = Regex("[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}")
+    val result = StringBuilder()
+    regex.find(date)?.value.let { normDate ->
+      normDate?.split("-")?.forEach { token ->
+        if (token.length == 1) {
+          result.append("0$token")
+        } else {
+          result.append(token)
+        }
+        result.append("-")
+      }
+    }
+    return result.substring(0, result.lastIndex).toString()
   }
 
-  fun dateToInt(date: LocalDateTime): Int {
-    return normalizeDate(date).replace("-", "")
-      .toInt()
+  fun dateToInt(date: String): Int {
+    return normalizeDate(date.toString()).replace("-", "").toInt()
   }
-
 }
--- app/src/test/kotlin/kotlin_playground/date/DateUtilsTest.kt
+++ app/src/test/kotlin/kotlin_playground/date/DateUtilsTest.kt
@@ -8,18 +8,32 @@ class DateUtilsTest {
 
   //2024-01-18T18:55:00.366190294
   //LocalDateTime.of(year = 2024, month = 1, dayOfMonth = 18, hour = 18, minute = 55, second = 0, nanoOfSecond = 366190294
-  val dateTo = LocalDateTime.of(2024, 1, 18, 18, 55, 0, 366190294)
+  private val dateTo: LocalDateTime = LocalDateTime.of(2024, 1, 8, 18, 55, 0, 366190294)
 
-  @Test
-  fun testNormalizeDate(){
-    val expected = "2024-01-18"
-    assertEquals(expected, DateUtils().normalizeDate(dateTo))
+  private val unsortedDates = listOf(
+    LocalDateTime.of(2024, 1, 18, 18, 55, 0, 366190294),
+    LocalDateTime.of(2024, 1, 18, 18, 55, 0, 366190294),
+    LocalDateTime.of(2024, 1, 18, 18, 55, 0, 366190294),
+    LocalDateTime.of(2024, 1, 18, 18, 55, 0, 366190294),
+    LocalDateTime.of(2024, 1, 18, 18, 55, 0, 366190294),
+    LocalDateTime.of(2024, 1, 18, 18, 55, 0, 366190294),
+    LocalDateTime.of(2024, 1, 18, 18, 55, 0, 366190294),
+    LocalDateTime.of(2024, 1, 18, 18, 55, 0, 366190294),
+    LocalDateTime.of(2024, 1, 18, 18, 55, 0, 366190294)
+  )
+
+  @Test fun testNormalizeDate() {
+    val expected = "2024-01-08"
+    assertEquals(expected, DateUtils().normalizeDate(dateTo.toString()))
+    assertEquals(expected, DateUtils().normalizeDate("2024-01-8"))
+    assertEquals(expected, DateUtils().normalizeDate("2024-1-08"))
 
   }
 
-  @Test
-  fun testDateToInt() {
-    val expected = 20240118
-    assertEquals(expected, DateUtils().dateToInt(dateTo))
+  @Test fun testDateToInt() {
+    val expected = 20240108
+    assertEquals(expected, DateUtils().dateToInt("2024-01-08"))
+    assertEquals(expected, DateUtils().dateToInt("2024-01-8"))
+    assertEquals(expected, DateUtils().dateToInt("2024-1-08"))
   }
 }
